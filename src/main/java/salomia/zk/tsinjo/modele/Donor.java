0ea90385879c58c1d139d64aa4363c1e1db9c97f
SalomiaZK <salomia.razakarivony@gmail.com>
2025-08-05 16:27:35 +0300

    fix: Donor boilerPlate

--- src/main/java/salomia/zk/tsinjo/modele/Donor.java
+++ src/main/java/salomia/zk/tsinjo/modele/Donor.java
@@ -3,10 +3,22 @@ package salomia.zk.tsinjo.modele;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
+
 public class Donor {
     private final String id;
     private final String name;
     private final String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
---
+++ src/test/java/salomia/zk/tsinjo/tests/MakeDonationTest.java
@@ -0,0 +1,10 @@
+package salomia.zk.tsinjo.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class MakeDonationTest {
+    @Test
+    public void make_donation_test() {
+
+    }
+}
